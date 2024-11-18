03794a9f8dbe7b9680cdd4d8f9e6eacf075740ff
Alexander Yerger <alexander.yerger@stmartin.edu>
2024-11-18 08:44:52 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Yerger_Alexander__Week_4_Project.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
